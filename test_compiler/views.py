4f016103c847859e4dcb54e635795efd549b277c
Maxim_Belov <belovmd@gmail.com>
2020-01-12 15:31:59 +0300

    Deploy test app

--- test_compiler/views.py
+++ test_compiler/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    return HttpResponse("maxim")
