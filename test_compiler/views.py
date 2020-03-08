ea5f7f760842b4fe195e86eb4f61277a29903fc9
Maxim_Belov <belovmd@gmail.com>
2020-03-08 12:01:14 +0300

    Fix dependency error

--- test_compiler/views.py
+++ test_compiler/views.py
@@ -7,8 +7,5 @@ def index(request):
     """
     Функция отображения для домашней страницы сайта.
     """
-    from django import forms
-    from codemirror import CodeMirrorTextarea
-
     form = SampleForm()
     return render(request, 'task.html', {"frm": form})
