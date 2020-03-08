25f1408ad6baf18028a748de7ddde73aa0a09373
Maxim_Belov <belovmd@gmail.com>
2020-03-08 15:11:29 +0300

    Add interaction

--- test_compiler/views.py
+++ test_compiler/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
 
 def index(request):
@@ -7,3 +7,13 @@ def index(request):
     Функция отображения для домашней страницы сайта.
     """
     return render(request, 'task.html')
+
+
+def test(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    content = ("program HelloWorld;\n"
+               "    begin writeln('Hello, world');\n"
+               "end.\n")
+    return JsonResponse({"data": content, "value": "bla"})
