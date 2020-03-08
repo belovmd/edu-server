933f9e7253c418db7f76d92a14b84fbccf858d05
Maxim_Belov <belovmd@gmail.com>
2020-03-08 12:04:32 +0300

    Fix dependency error

--- test_compiler/views.py
+++ test_compiler/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-from .forms import SampleForm
 
 def index(request):
     """
     Функция отображения для домашней страницы сайта.
     """
-    form = SampleForm()
-    return render(request, 'task.html', {"frm": form})
+    return render(request, 'task.html')
