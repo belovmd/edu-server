3e7cf188063bab4cec6cdd6e63273291e2072581
Maxim_Belov <belovmd@gmail.com>
2020-03-08 12:03:17 +0300

    Fix dependency error

--- test_compiler/forms.py
+++ test_compiler/forms.py
@@ -1,12 +1,1 @@
 from django import forms
-from codemirror import CodeMirrorTextarea
-
-
-widget = CodeMirrorTextarea(
-    mode="python",
-)
-
-
-class SampleForm(forms.Form):
-    # body = forms.CharField(label="Bodssy", widget=widget)
-    body = forms.CharField(label ="",  widget=widget)
