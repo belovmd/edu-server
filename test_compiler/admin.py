93c0395ba70029bfcafe082711c45a5c7d650bbd
Maxim_Belov <belovmd@gmail.com>
2020-03-16 00:23:32 +0300

    fix issue with textarea

--- test_compiler/admin.py
+++ test_compiler/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+@admin.register(models.Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('task_number', 'class_id', 'slug', 'task_title', 'task_description', 'task_snippet', 'author', )
+    prepopulated_fields = {'slug': ('task_number', 'class_id')}
+    ordering = ('class_id', 'task_number')
--- test_compiler/models.py
+++ test_compiler/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Task(models.Model):
+    task_number = models.CharField(max_length=30)
+    slug = models.SlugField(unique=True)
+    task_title = models.CharField(max_length=30, blank=True, null=True)
+    task_description = models.TextField()
+    task_snippet = models.TextField(blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    class_id = models.IntegerField(blank=True, null=True)
