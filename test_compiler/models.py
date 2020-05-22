6e436bad2ae18e42bbccc6c8eca1b61795e20a90
Maxim Belov <belovmd@gmail.com>
2020-05-22 17:47:24 +0300

    Rework fields for model

--- test_compiler/models.py
+++ test_compiler/models.py
@@ -7,10 +7,11 @@ class Task(models.Model):
     task_number = models.CharField(max_length=30)
     slug = models.SlugField(unique=True)
     task_title = models.CharField(max_length=30, blank=True, null=True)
-    task_description = models.TextField()
+    task_description = models.TextField(blank=True, null=True)
     task_snippet = models.TextField(blank=True, null=True)
     author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     class_id = models.IntegerField(blank=True, null=True)
+    paragraph = models.IntegerField(default=0)
 
     def get_absolute_url(self):
         return reverse('task_view', args=[self.slug])
