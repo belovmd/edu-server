e722efa7f94479a7e47df4c4beea10676dccb2e0
Maxim Belov <belovmd@gmail.com>
2020-05-23 12:50:41 +0300

    Change Slug Autogenerate

--- test_compiler/admin.py
+++ test_compiler/admin.py
@@ -6,5 +6,5 @@ from . import models
 @admin.register(models.Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('task_number', 'class_id', 'slug', 'task_title', 'task_description', 'task_snippet', 'author', )
-    prepopulated_fields = {'slug': ('task_number', 'class_id')}
+    prepopulated_fields = {'slug': ('task_number', 'class_id', 'paragraph')}
     ordering = ('class_id', 'task_number')
