b6d5cf369dfb6f715ee868d63d448050c9a80f8a
Maxim Belov <belovmd@gmail.com>
2020-05-23 20:42:17 +0300

    Update filtering

--- test_compiler/admin.py
+++ test_compiler/admin.py
@@ -5,9 +5,18 @@ from . import models
 
 @admin.register(models.Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('task_number', 'class_id', 'slug', 'task_title', 'task_description', 'task_snippet', 'author', )
+    list_display = ('task_number',
+                    'paragraph',
+                    'class_id',
+                    'slug',
+                    'task_title',
+                    'task_description',
+                    'task_snippet',
+                    'author', )
     prepopulated_fields = {'slug': ('task_number', 'class_id', 'paragraph')}
-    ordering = ('class_id', 'task_number')
+    ordering = ('class_id', 'paragraph', 'task_number')
+    list_filter = ('class_id', 'paragraph', 'class_homework')
+
 
 @admin.register(models.School)
 class SchoolAdmin(admin.ModelAdmin):
